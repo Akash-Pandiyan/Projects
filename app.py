35238c98f77b0594dff1385c22e2edacbbaeeae3
Akash-Pandiyan <125813905+Akash-Pandiyan@users.noreply.github.com>
2025-06-14 13:51:45 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Akash Pandiyan's ROSA Application!"
+    return "Hello from Akash Pandiyan"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
